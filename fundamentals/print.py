2d724c97c258618ddbd2deca671ecc90cc85c012
CoderAfnan2824 <coderafnan2428@gmail.com>
2025-12-12 00:22:30 +0530

    added fundamental: print, datatype

---
+++ fundamentals/print.py
@@ -0,0 +1,44 @@
+
+'''
+Docstring for fundamentals.print
+
+'''
+'''
+1. below is single line comment
+# single line comment
+
+2. below is multi line comment
+""" this is multi line comment
+this is second line"""
+
+'''
+
+
+# Below are all types of prints in Python
+
+print("Heloow world")
+
+print("name","afnan")
+
+print("name:", "afnan", 5)
+
+print("name: ","afnan", sep='-')
+
+print("name: ", end="")
+print("afnan")
+
+
+name = "afnan"
+age = 26
+
+print(f"my name is {name}. My age is {age}")
+
+
+print("my name is {}. age is {}".format(name,age))
+
+print(r"C:\new\folder") #this is raw string. no escape sequence is considered here
+
+print(''' this is 
+      multi line 
+      messsage''')
+
---
+++ fundamentals/variable_dataType.py
@@ -0,0 +1,17 @@
+# types of datatypes
+
+age = 25        # int
+name = "afnan"  # string
+gpa = 9.8       # float
+is_value = True # boolean
+
+
+# check type of the datatype
+print(type(age))    # prints int
+print(type(name))   # prints string
+
+
+# type casting: converting from one data type to other datatype
+
+age = float(age)    # comverted from int to float datatype
+print(type(age))
